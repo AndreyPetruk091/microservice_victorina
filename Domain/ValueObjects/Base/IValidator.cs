7cdba6af1eccaf78789890210c98e2e035f081ce
Пекарня <@DESKTOP-7I651UP>
2025-04-15 16:30:25 +0700

    Обновить Валидатор

---
+++ Domain/ValueObjects/Base/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMicroservice.Domain.ValueObjects.Base
+{
+    public interface IValidator<T>
+    {
+        
+        void Validate(T value);
+    }
+}
